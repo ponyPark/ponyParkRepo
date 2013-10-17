bf07fed00cc94d62dc4f6195fdc1768722bd5f4b
jlomeo <jlomeo@smu.edu>
2013-10-17 14:55:00 -0500

    Added Comment

--- src/userLogged.php
+++ src/userLogged.php
@@ -1,4 +1,9 @@
 <? 
+/*
+This page will check the session variable to see if a user is logged in.  
+If not, it will echo false.  Otherwise, it will echo true.  
+We need this so we can display the MyAccount/Favorites/Signout link on GUI end
 
+*/
 echo('false');
 ?>
