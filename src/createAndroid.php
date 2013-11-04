69d535006d17d7c999165bd7f8645c736f57461b
jlomeo <jlomeo@smu.edu>
2013-11-03 20:15:01 -0600

    Fixed Android Login with Dup email

--- src/createAndroid.php
+++ src/createAndroid.php
@@ -7,6 +7,14 @@ include 'phpapi.php';
 $phpInit = new phpapi();
 $phpInit->addUser();
 $phpInit->verifyUserAndroid();
-
+$phpInit = new phpapi();
+if (!$phpInit->addUser())
+{
+    echo('ERROR');
+}
+else
+{
+    $phpInit->verifyUserAndroid();
+}
 
 ?>
