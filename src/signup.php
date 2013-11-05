3e5e1b28b44264ea54ca5858eabd9863622b301e
JessicaYeh <jyeh@smu.edu>
2013-11-05 07:43:04 -0600

    Changed the look of the bad signup (duplicate email) message

--- src/signup.php
+++ src/signup.php
@@ -1,18 +1,14 @@
-<!-- PonyPark by BAM Software 
-
-	Will need to have a php script that checks to see if the user is logged in and redirect them to the my account page.
-
--->
+<!-- PonyPark by BAM Software -->
 
 
 <?
-
 session_start();
-if($_SESSION['logged'] === true){
+
+// Redirect the user to the home page if they are logged in.
+if($_SESSION['logged'] === true)
+{
     header ('Location: index.php');
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -32,16 +28,16 @@ if($_SESSION['logged'] === true){
 
 		<div class="hpContent" id="signInHP">
 
-		Already Registered? Log In.
-		<?
+    		<p class="details">Already Registered? Log In.</p>
+    		<?
                 if ($_GET["login"] === "false")
                     echo "<span id= 'badLogin' >Incorrect login information.<BR>Please try again.</span>";
             ?>
-			<form id="signInForm" method="POST" action="verify.php">
-				<!--Also will need option for fbook/twitter login once we get that far -->
-			<input type="email" name="email" title="Enter the email address associated with your account" placeholder="Email Address" required><input type="password" name="pw" placeholder = "Password" required>
-				<input type="submit" value="Log In" class="submit">
-			</form>
+    		<form id="signInForm" method="POST" action="verify.php">
+    		    <!--Also will need option for fbook/twitter login once we get that far -->
+    		    <input type="email" name="email" title="Enter the email address associated with your account" placeholder="Email Address" required><input type="password" name="pw" placeholder = "Password" required>
+    		    <input type="submit" value="Log In" class="submit">
+    		</form>
 
 		</div>
 
@@ -56,12 +52,11 @@ if($_SESSION['logged'] === true){
 		</nav>
 
 		<div class="hpContent" id="singUpHP">
-			<p class = "details">Use the signup form below to create a PonyPark Account.  You can also log in with your Facebook/Goggle Account.
-				<?php
-					if ($_GET['error'] == "true")
-						echo "<br><br><font color='#CC0000'>That email address is already in use.</font>";
-				?>
-			</p>
+			<p class = "details">Use the signup form below to create a PonyPark Account.</p>
+			<?php
+				if ($_GET['error'] === "true")
+					echo "<span id='badSignup'>That email address is already in use.</span>";
+			?>
 			<form id="createAccount" name="createAcct" method="POST" action="create.php">
                 <input type="text" name="fname" pattern="[a-zA-Z]+" title="Letters only" placeholder="First Name" required>
                 <input type="text" name="lname" placeholder="Last Name" title="Letters only" pattern="[a-zA-Z]+"required>
@@ -70,19 +65,14 @@ if($_SESSION['logged'] === true){
                 <input type="tel" name="phone" placeholder="Phone Number" pattern="[0-9]{10}" title="10 Digit Phone Number, Numbers only" required>
                 <input class="submitButton" type="submit" value="Sign Up" class="submit" id="cSub">
             </form>
-
-
-
-
 		</div>
-
-
 
 	</body>
 
 	<footer>
 		PonyPark | Southern Methodist University | Dallas, Texas 2013
-		<p id ="footerLinks"> <a href="http://nhl.com">Contact Us</a> <a href="http://nhl.com">Policies</a>
+		<p id ="footerLinks"> <a href="http://nhl.com">Contact Us</a> <a href="http://nhl.com">Policies</a></p>
+    </footer>
 
 </html>
 
