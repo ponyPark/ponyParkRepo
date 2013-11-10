75643eb48fe8751d60ed639519fcd66529cd53db
jlomeo <jlomeo@smu.edu>
2013-11-10 16:39:19 -0600

    Fixed include.js directory issue

--- src/js/includes.js
+++ src/js/includes.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
-    $("#logo").load("../logo.html");
-    $("#signIn").load("../dashboardSignIn.html");
-    $("#userOptions").load("../dashboardUserOptions.php");
-    $("#navigation").load("../navigation.html");
-    $("#footer").load("../footer.html");
+    $("#logo").load("logo.html");
+    $("#signIn").load("dashboardSignIn.html");
+    $("#userOptions").load("dashboardUserOptions.php");
+    $("#navigation").load("navigation.html");
+    $("#footer").load("footer.html");
 });
