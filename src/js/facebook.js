213be803110d7171363f305b8ef70d21898b259e
JessicaYeh <jyeh@smu.edu>
2013-11-11 23:37:13 -0600

    testing fb login

--- src/js/facebook.js
+++ src/js/facebook.js
@@ -21,7 +21,8 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
       $.ajax({
         type: "POST",
         url: "checkFacebookUser.php",
-        data: { "externalID":response.id }
+        data: { "email":response.email, "externalID":response.id, 
+                  "fname":response.first_name, "lname":response.last_name }
       });
     });
   } else if (response.status === 'not_authorized') {
@@ -32,36 +33,14 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
     // (1) JavaScript created popup windows are blocked by most browsers unless they 
     // result from direct interaction from people using the app (such as a mouse click)
     // (2) it is a bad experience to be continually prompted to login upon page load.
-   FB.login(function(response) {
-     if (response.authResponse) {
-       FB.api('/me', function(response) {
-          $.ajax({
-              type: "POST",
-              url: "addFacebookUser.php",
-              data: { "email":response.email, "externalID":response.id, 
-                  "fname":response.first_name, "lname":response.last_name }
-            });
-        });
-      }
-    });
+   FB.login();
   } else {
     // In this case, the person is not logged into Facebook, so we call the login() 
     // function to prompt them to do so. Note that at this stage there is no indication
     // of whether they are logged into the app. If they aren't then they'll see the Login
     // dialog right after they log in to Facebook. 
     // The same caveats as above apply to the FB.login() call here.
-    FB.login(function(response) {
-     if (response.authResponse) {
-       FB.api('/me', function(response) {
-          $.ajax({
-            type: "POST",
-            url: "addFacebookUser.php",
-            data: { "email":response.email, "externalID":response.id, 
-                "fname":response.first_name, "lname":response.last_name }
-            });
-          });
-        }
-      });
+    FB.login();
     }
   });
 };
--- src/phpapi.php
+++ src/phpapi.php
@@ -137,6 +137,8 @@ class phpapi
     }
 
     /**
+     * This function processes a Facebook sign in. If an entry doesn't already
+     * exist in the Users table for the user, then an entry is created.
      * It will set session variables to indicate a logged in user.
      * @return boolean True if user was successfully logged in through Facebook.
      * False if there was an error or if session variables were already set.
@@ -148,16 +150,35 @@ class phpapi
             !isset($_SESSION['userID']) || !isset($_SESSION['userName']))
         {
             // Obtain user info
+            $fname = mysql_real_escape_string($_POST['fname']);
+            $lname = mysql_real_escape_string($_POST['lname']);
+            $email = mysql_real_escape_string($_POST['email']);
             $externalID = mysql_real_escape_string($_POST['externalID']);
+            $auth = 0;
             $externalType = "Facebook";
 
-            if(empty($externalID))
+            if(empty($email) || empty($externalID))
                 return false;
 
             //Query to see if the user already exists
             $query = "SELECT * FROM Users WHERE ExternalID = '$externalID' AND 
                 ExternalType = '$externalType'";
             $result = mysql_query($query);
+
+            //If the user doesn't exist.
+            if(mysql_num_rows($result)==0)
+            {
+                //Add the info into the users table.
+                $query = "INSERT INTO Users(FirstName, LastName, Email, UserType,
+                    ExternalType, ExternalID) VALUES ('$fname','$lname','$email','$auth',
+                    '$externalType', '$externalID')";
+                mysql_query($query);
+                
+                //Get everything from the row just inserted.
+                $query = "SELECT * FROM Users WHERE ExternalID = '$externalID' AND 
+                    ExternalType = '$externalType'";
+                $result = mysql_query($query);
+            }
 
             $info = mysql_fetch_array($result);
             $_SESSION['logged'] = true;
@@ -170,47 +191,6 @@ class phpapi
 
             return true;
         }
-        return false;
-    }
-
-    /**
-     * This function processes a Facebook sign in. If an entry doesn't already
-     * exist in the Users table for the user, then an entry is created.
-     * @return boolean True if user was successfully added to the database 
-     * through Facebook. False if there was an error.
-     */
-    public function addFacebookUser()
-    {
-        $query = "INSERT INTO Ratings (ParkingID, UserID, Timestamp, Rating, Level) values (1, 14, NOW(), 1, 1)";
-        mysql_query($query);
-
-        // Obtain user info
-        $fname = mysql_real_escape_string($_POST['fname']);
-        $lname = mysql_real_escape_string($_POST['lname']);
-        $email = mysql_real_escape_string($_POST['email']);
-        $externalID = mysql_real_escape_string($_POST['externalID']);
-        $auth = 0;
-        $externalType = "Facebook";
-
-        if(empty($email) || empty($externalID))
-            return false;
-
-        //Query to see if the user already exists
-        $query = "SELECT * FROM Users WHERE ExternalID = '$externalID' AND 
-            ExternalType = '$externalType'";
-        $result = mysql_query($query);
-
-        //If the user doesn't exist.
-        if(mysql_num_rows($result)==0)
-        {
-            //Add the info into the users table.
-            $query = "INSERT INTO Users(FirstName, LastName, Email, UserType,
-                ExternalType, ExternalID) VALUES ('$fname','$lname','$email','$auth',
-                '$externalType', '$externalID')";
-            mysql_query($query);
-            return true;
-        }
-
         return false;
     }
 
