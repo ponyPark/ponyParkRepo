30e8e1e30b0b46cf52707d4f553d4e734f340ace
jlomeo <jlomeo@smu.edu>
2013-11-13 20:32:10 -0600

    All links are active and point to real pages except Request

---
+++ src/contact.php
@@ -0,0 +1,42 @@
+<!-- PonyPark by BAM Software -->
+
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>PonyPark | Trot Right In</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<script type="text/javascript" src="js/script.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/includes.js"></script>
+		<script type="text/javascript" src="js/google.js"></script>
+		<script type="text/javascript" src="js/facebook.js"></script>
+	</head>
+	<body>
+        <div id="logo"><!-- logo.html --></div>
+
+		<section id="signIn"><!-- dashboardSignIn.html --></section>
+		<div id="userOptions"><!-- dashboardUserOptions.php --></div>
+		<nav id="navigation"><!-- navigation.html --></nav>
+
+		<div class="hpContent" id = "whatsPonyPark">
+			<h2>Contact PonyPark</h2>
+			<p class = "details">Want to get ahold of PonyPark?  Please send all inquiries to
+
+				<p>Southern Methodist University</p>
+				<p>Attn: PonyPark</p>
+				<p>3052 Boaz Lane</p>
+				<p>Dallas, Texas 75205</p>
+
+			</p>
+			
+		</div>
+
+	</body>
+
+	<footer id="footer"><!-- footer.html --></footer>
+
+</html>
+
