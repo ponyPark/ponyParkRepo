e34300d5ce4fcd22c861f7d1a46fb73ed00c72c8
SZanetti <storyzanetti@gmail.com>
2013-11-15 15:56:44 -0600

    Edited notifyUser

--- src/phpapi.php
+++ src/phpapi.php
@@ -725,11 +725,14 @@ class phpapi
                 ParkingLocations.ParkingID ORDER BY timestamp DESC LIMIT 1) AS 
                 Last_Rated FROM ParkingLocations JOIN FavoriteGarages WHERE
                 ParkingLocations.parkingID = FavoriteGarages.parkingID AND
-                FavoriteGarages.UserID =" . $row['UserID'] . "ORDER BY 
+                FavoriteGarages.UserID = " . '$row['UserID']' . " ORDER BY 
                 FavoriteGarages.Priority";
-            $result = mysql_query($query);
+            $result2 = mysql_query($query);
 
-            echo $result;
+            while ($row2 = mysql_fecth_assoc($result2))
+            {
+                var_dump($row2);
+            }
         }
     }
 }
