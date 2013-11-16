6b819ca4199cc94633fca177e0c1996c7e1da134
jlomeo <jlomeo@smu.edu>
2013-11-16 14:51:01 -0600

    Request Status Page Completed

---
+++ src/js/requestStat.js
@@ -0,0 +1,99 @@
+/*BAM Software JS for PonyPark
+October, 2013*/
+function init() {
+
+    function userLogged() {
+        var request = new XMLHttpRequest();
+        var url = 'userLogged.php';
+        var data;
+
+        request.open("GET", url, true);
+        request.send();
+        request.onreadystatechange = function (e) {
+
+            if (request.readyState === 4) {
+                //save the response from server
+                //if userLogged.php outputs false, then the signin will display
+                //if userLogged.php outputs true, then the favorites, logout, and manage account will display
+                data = request.responseText;
+                if (data === "true") {
+                    $('#signIn').css('display', 'none');
+                    $('#userOptions').css('display', 'block');
+                }
+                if (data === "false") {
+                    $('#signIn').css('display', 'block');
+                    $('#userOptions').css('display', 'none');
+                }
+
+            }
+        }
+
+
+    }
+
+    function loadList(){
+        var request = new XMLHttpRequest();
+        var url = 'getRequests.php';
+        var data;
+
+        request.open("GET", url, true);
+        request.send();
+        request.onreadystatechange = function (e) {
+
+            if (request.readyState === 4) {
+                data = request.responseText;
+                data = JSON.parse(data);
+                data = data.RequestedGarages;
+                var count = data.length;
+                for(var i =0; i < count; i++){
+                    var status;
+                    var cost = data[i].Cost;
+                    var comments = data[i].Comments;
+                    if(data[i].Status === '0'){
+                        status = "Pending";
+                    }
+                    if(data[i].Status === '1'){
+                        status = "Denied";
+                    }
+                    if(data[i].Status === '2'){
+                        status = "Approved";
+                    }
+                    if(data[i].Cost === null){
+                        cost = "None Specified";
+                    }
+                    if(data[i].Comments === null){
+                        comments = "None Entered";
+                    }
+                        var parentList = $('#requestStatus');
+                        var child1 = $('<li />', {text: "Name of Garage: " + data[i].Name});
+                        var child1child1 = $('<ul />');
+                        var child1child1child = $('<li />', {text: "Address: " + data[i].Address});
+                        child1child1.append(child1child1child);
+                        var child1child1child2 = $('<li />', {text: "Cost: " + cost});
+                        child1child1.append(child1child1child2);
+                        var child1child1child3 = $('<li />', {text: "Levels: " + data[i].NumberOfLevels});
+                        child1child1.append(child1child1child3);
+                        var child1child1child4 = $('<li />', {text: "Comments: " + comments});
+                        child1child1.append(child1child1child4);
+                        var child1child1child5 = $('<li />', {text: "Status: " + status});
+                        child1child1.append(child1child1child5);
+                        child1.append(child1child1);
+
+
+
+
+                        parentList.append(child1);
+                    }
+                }
+
+                if(count === 0){
+                    $('p.details').text("You have not made any requests.");
+                }
+            }
+        }
+
+    
+    loadList();
+    userLogged();
+}
+window.onload = init;
--- src/requestStat.php
+++ src/requestStat.php
@@ -7,7 +7,7 @@
 		<title>PonyPark | Trot Right In</title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
-		<script type="text/javascript" src="js/script.js"></script>
+		<script type="text/javascript" src="js/requestStat.js"></script>
 		<link href='http://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script type="text/javascript" src="js/includes.js"></script>
@@ -15,30 +15,33 @@
 		<script type="text/javascript" src="js/facebook.js"></script>
 	</head>
 	<body>
+
+
+		<?
+
+				session_start();
+				if($_SESSION['logged'] != true){
+				    header ('Location: index.php');
+				}
+
+						?>
+
+
+
+
         <div id="logo"><!-- logo.html --></div>
 
 		<section id="signIn"><!-- dashboardSignIn.html --></section>
 		<div id="userOptions"><!-- dashboardUserOptions.php --></div>
 		<nav id="navigation"><!-- navigation.html --></nav>
 
-		<div class="hpContent">
-			<h2>Manage Account</h2>
-			<p class = "details">Please choose an action you would like to perform:
-				<ul>
-					<? session_start();
-			if($_SESSION['userType'] == 1 && $_SESSION['logged'] == true) echo("<li><a href='admin.php'>Admin Dashboard</a></li>"); ?>
-					<li><a href="editfav.php">Add/Remove Favorites</a></li>
-					<li>Notification Settings
-						<UL>
-							<LI><a href="ctimes.php">Add Notification Time</a></LI>
-							<LI><a href="Ectimes.php">Edit/Delete Notification Times</a></LI>
-
-						</UL></li>
-					<li><a href="request.php">Request Addition of a Garage to PonyPark</a></li>
-					<li><a href="requestStat.php">Request Status</a></li>
-					
-				</ul></p>
+		<div class="hpContent" id = "whatsPonyPark">
+			<h2>Request Status</h2>
+			<p class = "details">
+				The status of your garage addition(s) are below:</p>
+			<UL id="requestStatus"></UL>
 		</div>
+
 	</body>
 
 	<footer id="footer"><!-- footer.html --></footer>
