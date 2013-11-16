80289ec3c7521047153e1689f974e0a96376a259
jlomeo <jlomeo@smu.edu>
2013-11-16 12:53:35 -0600

    Top Ten Users Full Functional"

---
+++ src/js/topTen.js
@@ -0,0 +1,65 @@
+/*BAM Software JS for PonyPark
+October, 2013*/
+function init() {
+
+    function userLogged() {
+        var request = new XMLHttpRequest();
+        var url = 'userLogged.php';
+        var data;
+
+        request.open("GET", url, true);
+        request.send();
+        request.onreadystatechange = function (e) {
+
+            if (request.readyState === 4) {
+                //save the response from server
+                //if userLogged.php outputs false, then the signin will display
+                //if userLogged.php outputs true, then the favorites, logout, and manage account will display
+                data = request.responseText;
+                if (data === "true") {
+                    $('#signIn').css('display', 'none');
+                    $('#userOptions').css('display', 'block');
+                    $('nav ol li:first-child a').attr("href", "index.php");
+                    $('nav ol li:first-child a').text("Availability");
+                }
+                if (data === "false") {
+                    $('#signIn').css('display', 'block');
+                    $('#userOptions').css('display', 'none');
+                }
+
+            }
+        }
+
+
+    }
+
+    function loadList(){
+        var request = new XMLHttpRequest();
+        var url = 'getTop10Users.php';
+        var data;
+
+        request.open("GET", url, true);
+        request.send();
+        request.onreadystatechange = function (e) {
+
+            if (request.readyState === 4) {
+                data = request.responseText;
+                data = JSON.parse(data);
+                data = data.TopTen;
+                for(var i =0; i < data.length; i++){
+                        var parentList = $('#topTenList');
+                        var child1 = $('<li />', {text: data[i].FirstName + " " + data[i].LastName + " - " + data[i].Points + " Points"});
+                        parentList.append(child1);
+                }
+            }
+        }
+
+
+
+    }
+
+    
+    loadList();
+    userLogged();
+}
+window.onload = init;
---
+++ src/topten.php
@@ -0,0 +1,35 @@
+<!-- PonyPark by BAM Software -->
+
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>PonyPark | Trot Right In</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<script type="text/javascript" src="js/topTen.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/includes.js"></script>
+		<script type="text/javascript" src="js/google.js"></script>
+		<script type="text/javascript" src="js/facebook.js"></script>
+	</head>
+	<body>
+        <div id="logo"><!-- logo.html --></div>
+
+		<section id="signIn"><!-- dashboardSignIn.html --></section>
+		<div id="userOptions"><!-- dashboardUserOptions.php --></div>
+		<nav id="navigation"><!-- navigation.html --></nav>
+
+		<div class="hpContent">
+			<h2>Top Ten Users</h2>
+			<p class = "details">At PonyPark, we value those who help make our service a success.  Here are our top ten contributers.  Please, aspire to be like these individuals and contribute!</p>
+			<OL id="topTenList"></OL>
+		</div>
+
+	</body>
+
+	<footer id="footer"><!-- footer.html --></footer>
+
+</html>
+
