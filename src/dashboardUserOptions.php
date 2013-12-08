9fffb6413eb2d89a5c2e7908d7606a257ade9569
JessicaYeh <jyeh@smu.edu>
2013-12-07 21:34:20 -0600

    changed font size of welcome message

--- src/dashboardUserOptions.php
+++ src/dashboardUserOptions.php
@@ -1,9 +1,10 @@
-<h4>Welcome back to PonyPark, <? session_start(); echo($_SESSION['userName']);?>!</h4>
+<font class="biggerFontForStory">Welcome back to PonyPark, <? session_start(); echo($_SESSION['userName']);?>!</h4>
 <ol id="userList">
     <li><a href="maccount.php">Manage Account</a></li>
     <li><a href="favlist.php">Favorite List</a></li>
     <li><a href="#" id="signOut">Sign Out</a></li>
 </ol>
+</font>
 
 <script>
     $("#signOut").click(function() {
